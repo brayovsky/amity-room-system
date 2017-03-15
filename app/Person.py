31f00eeeae761b5f1fde09604a2d7794e3bbb19d
brayovsky <brian.muthui@andela.com>
2017-03-15 19:17:58 +0300

    [feature 141766583] Add function allocate rooms

--- app/Person.py
+++ app/Person.py
@@ -1,27 +1,49 @@
 class Person:
-    def __init__(self):
-        pass
+    def __init__(self, person_name):
+        self.name = person_name
 
-    def book_office(self):
+    def book_office(self, allocations, all_rooms):
+        print("Booking office for {}".format(self.name))
+
+        """
+        Books a person into an office
+        :param office_name: Office to book into
+        :return:
+        """
         pass
 
     def evacuate_room(self):
+        """
+        Evacuates a person from a room. Used when changing rooms
+        :param room_name: The room to evacuate from
+        :return: True or False
+        """
         pass
 
-    def change_room(self):
+    def change_room(self, old_room, room_type):
+        """
+        Changes a room
+        :param new_room: Room to move from
+        :param room_type: Office or livingspace
+        :return: True or False
+        """
         pass
 
 
 class Staff(Person):
-    def __init__(self, amity, person_name):
-        super(Staff, self).__init__(amity, person_name)
+    def __init__(self, person_name):
+        super(Staff, self).__init__(person_name)
         pass
 
 
 class Fellow(Person):
-    def __init__(self, amity, person_name):
-        super(Fellow, self).__init__(amity, person_name)
+    def __init__(self, person_name):
+        super(Fellow, self).__init__(person_name)
         pass
 
-    def book_living_space(self):
-        pass
+    def book_living_space(self, allocations, livingspaces):
+        """
+        Books a fellow into a living space
+        :return: True or False
+        """
+        print("Booking living space for {}".format(self.name))
--- app/Rooms.py
+++ app/Rooms.py
@@ -1,6 +1,5 @@
 class Room:
     def __init__(self, amity, room_name):
-        self.max_no_of_occupants = 0
         pass
 
     def assign_occupant(self, person_name):
@@ -16,10 +15,10 @@ class Room:
 class Office(Room):
     def __init__(self, amity, room_name):
         super(Office, self).__init__(amity, room_name)
-        pass
+        self.max_no_of_occupants = 6
 
 
 class LivingSpace(Room):
     def __init__(self, amity, room_name):
         super(LivingSpace, self).__init__(amity, room_name)
-        pass
+        self.max_no_of_occupants = 4
