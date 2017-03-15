36972b9c14c82d66655742f8cdb51a0e35e97898
brayovsky <brian.muthui@andela.com>
2017-03-15 08:30:05 +0300

    [feature 141676949] Add functionality for adding fellow - Make sure duplicate room names are not added to the system

--- app/Amity.py
+++ app/Amity.py
@@ -8,6 +8,7 @@ class Amity:
         self.people = {"fellows": set(),
                        "staff": set()
                        }
+        self.people_to_be_accommodated = set()
         self.allocated_rooms = {}
 
     def add_room(self, room_names, room_type):
@@ -21,16 +22,22 @@ class Amity:
         else:
             opposite_room = "offices"
 
-        new_set = set_of_new_rooms.union(self.rooms[opposite_room])
+        new_set = set_of_new_rooms | self.rooms[opposite_room]
         expected_length = len(self.rooms[opposite_room]) + len(set_of_new_rooms)
 
         if len(new_set) < expected_length:
             # Duplicate exists
-            print("There are rooms that already exist. Please try again with new names")
-            return
+            print("There are rooms that already exist. Duplicate rooms will be removed")
+
+            # Get duplicates and remove them from set_of_new_rooms
+            all_rooms = self.rooms["livingspaces"] | self.rooms["offices"]
+            duplicate_rooms = set_of_new_rooms & all_rooms
+
+            for duplicate_room in duplicate_rooms:
+                set_of_new_rooms.discard(duplicate_room)
 
         # Add new rooms
-        self.rooms[room_type] = self.rooms[room_type].union(set_of_new_rooms)
+        self.rooms[room_type] |= set_of_new_rooms
         print("Rooms added succesfully")
         self.total_no_of_rooms = len(self.rooms["offices"]) + len(self.rooms["livingspaces"])
         self.print_allocations()
@@ -38,24 +45,47 @@ class Amity:
     def add_person(self, person_name, person_type, wants_accommodation=False):
         """Adds a person into amity"""
 
+        # Check for duplicates
+
         # Staff should not have accommodation
         if person_type == "staff" and wants_accommodation:
             print("Staff cannot have accommodation")
 
         person = {person_name, }
-        self.people[person_type] = self.people[person_type].union(person)
+
+        if person_type == "staff":
+            self.people[person_type] |= person
+        elif person_type == "fellows":
+            self.people[person_type] |= person
+            # Add to list of accommodations
+            if wants_accommodation:
+                self.people_to_be_accommodated.add(person_name)
+
         self.total_no_of_people = len(self.people["fellows"]) + len(self.people["staff"])
 
+        self.print_allocations()
 
     def load_people(self, filename):
         pass
 
     def print_allocations(self, filename=None):
+        print("--------------------------------------------------")
+        print("All rooms in amity")
+        print("--------------------------------------------------")
         print(self.rooms)
+        print("--------------------------------------------------")
+        print("All people in Amity")
+        print("--------------------------------------------------")
         print(self.people)
+        print("--------------------------------------------------")
+        print("Fellows to be accommodated")
+        print("--------------------------------------------------")
+        print(self.people_to_be_accommodated)
+        print("--------------------------------------------------")
         print("Total number of people is {0}".format(self.total_no_of_people))
+        print("--------------------------------------------------")
         print("Total number of rooms is {0}".format(self.total_no_of_rooms))
-
+        print("--------------------------------------------------")
 
     def print_unallocated(self, filename=None):
         pass
