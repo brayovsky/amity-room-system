0239d3cf408fad217c1bd9e43fa34bef5558afa3
brayovsky <brian.muthui@andela.com>
2017-03-20 13:38:52 +0300

    [feature 141687115] Create correct database structure - tables, columns and relationships

--- app/Model.py
+++ app/Model.py
@@ -5,20 +5,21 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-
-class Address(Base):
-    __tablename__ = 'address'
+class Rooms(Base):
+    __tablename__ = 'rooms'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    room_name = Column(String(250), primary_key=True)
+    room_type = Column(String(1), nullable=False)
 
+
+class People(Base):
+    __tablename__ = 'people'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    # id = Column(Integer, primary_key=True)
+    person_name = Column(String(250), primary_key=True)
+    person_type = Column(String(1), nullable=False)
+    office = Column(String(250), ForeignKey('rooms.room_name'), nullable=True)
+    living_space = Column(String(250), ForeignKey('rooms.room_name'), nullable=True)
+    room = relationship(Rooms)
